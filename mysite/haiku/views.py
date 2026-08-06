73c274ce263ee3896ff11f3a708dac2190f20581
urabe shuya <urabeshuya1226@gmail.com>
2026-08-06 16:13:01 +0900

    index関数追加

--- mysite/haiku/views.py
+++ mysite/haiku/views.py
@@ -1,7 +1,18 @@
 import random
-from django.http import HttpResponse
-from .model import Kigo
+from django.shortcuts import render
+from .models import Kigo
+from .forms import HaikuForm
+
 def index(request):
-  Kigo=random.choice(list(Kigo.objects.all()))
-  return HttpResponse(f"お題:{kigo.word}
-  ({Kigo.get_season_display()}・{kigo.get_genre_display()})")
+  if request.method == "POST":
+    kigo = Kigo.objects.get(pk=request.POST.get("kigo_id"))
+    form = HaikuForm(request.POST)
+    if form.is_valid():
+      haiku = form.save(commit=False)
+      haiku.kigo = kigo
+      haiku.save()
+  else:
+    kigo = random.choice(list(Kigo.objects.all()))
+    form = HaikuForm()
+
+  return render(request, "haiku/index.html", {"kigo": kigo, "form": form})
